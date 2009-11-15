3415ae4961a0d2f912d5307150ddea14327ec87f
Leandro de Oliveira <ol.leandro@gmail.com>
2009-11-15 17:31:16 -0200

    Changes user migration template

--- generators/flexi_authentication/templates/migration.rb
+++ generators/flexi_authentication/templates/migration.rb
@@ -12,6 +12,16 @@ class Create<%= user_plural_class_name %> < ActiveRecord::Migration
       t.string :password_salt
       t.timestamps
     end
+    
+    <%- if options[:authlogic] -%>
+      # ...
+	<%- else -%>	    	
+	execute "INSERT INTO <%= user_plural_name %> (username,email,password_hash,password_salt) VALUES ('admin','admin@admin.com','3dad6da7754a5de4c3d46fc3901adf1a1ae9ce18','4207a4c0006e0b01da91693066d91a5e34fa7fb9')"
+	  puts ""
+	  puts "Username: admin"
+	  puts "Password: admin123"
+	  puts ""
+    <%- end -%>	
   end
   
   def self.down

28a1c107938ea3f4128940f51e36aa75258608a4
Leandro de Oliveira <ol.leandro@gmail.com>
2009-11-15 17:54:33 -0200

    Fix redirect

--- generators/flexi_authentication/templates/sessions_controller.rb
+++ generators/flexi_authentication/templates/sessions_controller.rb
@@ -12,7 +12,7 @@ class <%= session_plural_class_name %>Controller < ApplicationController
     @<%= session_singular_name %> = <%= session_class_name %>.new(params[:<%= session_singular_name %>])
     if @<%= session_singular_name %>.save
       flash[:notice] = "Bem vindo!"
-      redirect_to_target_or_default(root_url)
+      redirect_to admin_home_url
     else
       flash.now[:error] = "Login ou senha inválidos."
       render :action => 'new'

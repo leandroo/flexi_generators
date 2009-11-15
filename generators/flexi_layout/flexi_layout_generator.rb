c95bfadabe2df13882fdbe5915ecfad28eb21acc
Leandro de Oliveira <ol.leandro@gmail.com>
2009-11-15 17:29:25 -0200

    Changes layout generator

--- generators/flexi_layout/flexi_layout_generator.rb
+++ generators/flexi_layout/flexi_layout_generator.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.dirname(__FILE__) + "/lib/insert_commands.rb")
+
 class FlexiLayoutGenerator < Rails::Generator::Base
   
   def manifest
@@ -6,16 +8,31 @@ class FlexiLayoutGenerator < Rails::Generator::Base
       m.directory 'public/stylesheets'
       m.directory 'public/images'
       m.directory 'app/helpers'
+      m.directory 'app/views/home'
+      m.directory 'app/views/admin/home'
+      m.directory 'app/controllers/admin'
       m.template "layout.html.erb", "app/views/layouts/application.html.erb"
       m.template "layout_login.html.erb", "app/views/layouts/login.html.erb"
+	  m.template "layout_site.html.erb", "app/views/layouts/site.html.erb"
+	  m.template "home_index.html.erb", "app/views/home/index.html.erb"
+	  m.template "admin_home_index.html.erb", "app/views/home/admin/index.html.erb"	  
+	  m.file     "admin_home_controller.rb", "app/controllers/admin/home_controller.rb"	  
+	  m.file     "home_controller.rb", "app/controllers/home_controller.rb"
+	  m.file     "home_helper.rb", "app/helpers/home_helper.rb"	  	  	  
       m.file     "stylesheet.css",  "public/stylesheets/application.css"
       m.file     "stylesheet_login.css",  "public/stylesheets/login.css"
+	  m.file     "stylesheet_site.css",  "public/stylesheets/site.css"      
       m.file     "logo.png",  "public/images/logo.png"
       m.file     "bg_cinza.png",  "public/images/bg_cinza.png"
       m.file     "helper.rb", "app/helpers/layout_helper.rb"
       m.file     "custom_field_error.rb", "config/initializers/custom_field_error.rb"
       m.file     "inflector_portuguese.rb", "config/initializers/inflector_portuguese.rb"
       m.file     "pt-BR.yml", "config/locales/pt-BR.yml"
+      m.route    "map.root :controller => :home"
+      m.route    "map.admin 'admin', :controller => 'sessions', :action => 'new'"
+      m.route    "map.namespace :admin do |admin|
+  	admin.home 'home', :controller => 'home', :action => 'index'
+  end"             
     end
   end
   

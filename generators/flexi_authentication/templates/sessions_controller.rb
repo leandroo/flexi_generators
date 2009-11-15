8098a1991fcde0a25dddce3412262169262d5925
Leandro de Oliveira <ol.leandro@gmail.com>
2009-11-15 00:59:20 -0200

    changes sessions_controller template

--- generators/flexi_authentication/templates/sessions_controller.rb
+++ generators/flexi_authentication/templates/sessions_controller.rb
@@ -1,5 +1,9 @@
 class <%= session_plural_class_name %>Controller < ApplicationController
 <%- if options[:authlogic] -%>
+  skip_before_filter :login_required
+  
+  layout 'login'
+  	
   def new
     @<%= session_singular_name %> = <%= session_class_name %>.new
   end
@@ -7,9 +11,10 @@ class <%= session_plural_class_name %>Controller < ApplicationController
   def create
     @<%= session_singular_name %> = <%= session_class_name %>.new(params[:<%= session_singular_name %>])
     if @<%= session_singular_name %>.save
-      flash[:notice] = "Logged in successfully."
+      flash[:notice] = "Bem vindo!"
       redirect_to_target_or_default(root_url)
     else
+      flash.now[:error] = "Login ou senha inválidos."
       render :action => 'new'
     end
   end
@@ -17,10 +22,12 @@ class <%= session_plural_class_name %>Controller < ApplicationController
   def destroy
     @<%= session_singular_name %> = <%= session_class_name %>.find
     @<%= session_singular_name %>.destroy
-    flash[:notice] = "You have been logged out."
+    flash[:notice] = "Você foi desconectado."
     redirect_to root_url
   end
 <%- else -%>
+  skip_before_filter :login_required
+  
   layout 'login'
   
   def new
@@ -30,7 +37,7 @@ class <%= session_plural_class_name %>Controller < ApplicationController
     <%= user_singular_name %> = <%= user_class_name %>.authenticate(params[:login], params[:password])
     if <%= user_singular_name %>
       session[:<%= user_singular_name %>_id] = <%= user_singular_name %>.id
-      flash[:notice] = "Logged in successfully."
+      flash[:notice] = "Bem vindo!"
       redirect_to_target_or_default(root_url)
     else
       flash.now[:error] = "Login ou senha inválidos."
@@ -40,7 +47,7 @@ class <%= session_plural_class_name %>Controller < ApplicationController
   
   def destroy
     session[:<%= user_singular_name %>_id] = nil
-    flash[:notice] = "You have been logged out."
+    flash[:notice] = "Você foi desconectado."
     redirect_to root_url
   end
 <%- end -%>

5a630efc3be5dc6ac6c1d30be8f953684e3ab486
Leandro de Oliveira <ol.leandro@gmail.com>
2009-11-17 00:10:20 -0200

    Fix source

--- flexi_template.rb
+++ flexi_template.rb
@@ -11,7 +11,7 @@ plugin "jrails", :git => "git://github.com/aaronchi/jrails.git"
 plugin "flexi_generators", :git => "git://github.com/leandroo/flexi_generators.git"
 
 # gems
-gem 'mislav-will_paginate', :lib=>'will_paginate', :version=> '2.3.11'
+gem 'will_paginate', :version=> '2.3.11', :source => 'http://gemcutter.org'
 
 # locales
 environment %(config.i18n.default_locale = "pt-BR")

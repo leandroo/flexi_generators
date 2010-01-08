b72905f305ffb4cce92b0063aff83b617c6e009a
Leandro de Oliveira <ol.leandro@gmail.com>
2010-01-07 23:02:39 -0200

    User model fixed

--- generators/flexi_authentication/templates/user.rb
+++ generators/flexi_authentication/templates/user.rb
@@ -13,7 +13,7 @@ class <%= user_class_name %> < ActiveRecord::Base
   validates_format_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validates_presence_of :password, :on => :create
-  validates_presence_of :password_confirmation
+  validates_presence_of :password_confirmation, :on => :create
   validates_confirmation_of :password
   validates_length_of :password, :minimum => 4, :allow_blank => true
   

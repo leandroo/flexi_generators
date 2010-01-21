d64d1264ba4675f36085653d6268ea3f1ed86987
Leandro de Oliveira <ol.leandro@gmail.com>
2010-01-21 00:26:37 -0200

    Modified flash notice

--- generators/flexi_scaffold/templates/actions/destroy.rb
+++ generators/flexi_scaffold/templates/actions/destroy.rb
@@ -1,6 +1,6 @@
   def destroy
     @<%= singular_name %> = <%= class_name %>.find(params[:id])
     @<%= singular_name %>.destroy
-    flash[:notice] = "<%= singular_name.capitalize %> excluída com sucesso!."
+    flash[:notice] = "<%= singular_name.capitalize %> excluída com sucesso!"
     redirect_to <%= items_path('url') %>
   end

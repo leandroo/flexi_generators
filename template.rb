ce2f12044649cef4a5a860f9d15ca6b70412da5b
Leandro de Oliveira <ol.leandro@gmail.com>
2011-04-20 23:10:12 -0300

    Update app template

--- template.rb
+++ template.rb
@@ -1,15 +1,5 @@
-# Check prerequisites
-%w{bundler flexi_generators}.each do |component|
-  unless Gem.available?(component)
-    run "gem install #{component}"
-    Gem.refresh
-    Gem.activate(component)
-  end
-end
-# Add flexi_generators to Gemfile
 gem "flexi_generators", "0.2.10"
-# Flexi Genertors
+run "bundle install"
 generate 'flexi:prepare'
 generate 'flexi:auth'
-# Bundle
 run "bundle install"

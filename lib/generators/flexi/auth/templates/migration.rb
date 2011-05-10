eebda598bcdcdd5b48a04777d877949b4d957c0f
Leandro de Oliveira <ol.leandro@gmail.com>
2011-05-10 01:52:33 -0300

    Add authlogic magic columns

--- lib/generators/flexi/auth/templates/migration.rb
+++ lib/generators/flexi/auth/templates/migration.rb
@@ -6,6 +6,13 @@ class Create<%= user_plural_class_name %> < ActiveRecord::Migration
       t.string :persistence_token
       t.string :crypted_password
       t.string :password_salt
+      t.integer :login_count, :default => 0, :null => false
+      t.integer :failed_login_count, :default => 0, :null => false
+      t.datetime :last_request_at
+      t.datetime :current_login_at
+      t.datetime :last_login_at
+      t.string :current_login_ip
+      t.string :last_login_ip      
       t.timestamps
     end
   end

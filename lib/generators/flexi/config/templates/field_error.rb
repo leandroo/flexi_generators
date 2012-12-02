ac0066d521c6b80aa818be09885507c3eda66bc6
Leandro de Oliveira <ol.leandro@gmail.com>
2012-12-02 15:41:56 -0200

    Field errors updated

--- lib/generators/flexi/config/templates/field_error.rb
+++ lib/generators/flexi/config/templates/field_error.rb
@@ -1,9 +1,14 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  if html_tag =~ /<(input|textarea|select)/
-    %|<div class="control-group error">#{html_tag} <span class="help-inline">#{[instance.error_message].join(', ')}</span></div>|.html_safe
-  elsif html_tag =~ /<label/
-    %|<div class="control-group error">#{html_tag}|.html_safe
-  else
-    html_tag
+  html = html_tag.html_safe
+  elements = Nokogiri::HTML::DocumentFragment.parse(html_tag).css "label, input, select, textarea"
+  elements.each do |e|
+    if e.node_name.eql? 'label'
+      e['class'] = 'control-label error'
+      html = %(#{e}).html_safe
+    else
+      e['class'] = "#{e['class']} error"
+      html = %(#{e}).html_safe
+    end
   end
+  html
 end

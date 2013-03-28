f27e0e28115254bcf6eb552664a272859d54ec20
Leandro de Oliveira <ol.leandro@gmail.com>
2013-03-28 20:23:58 -0300

    Changes layout templates

--- lib/generators/flexi/layout/templates/error_messages_helper.rb
+++ lib/generators/flexi/layout/templates/error_messages_helper.rb
@@ -9,7 +9,7 @@ module ErrorMessagesHelper
         # forced as error but may not abstractly always be the case
       content_tag(:div, :class => "alert alert-danger") do
         list_items = messages.map { |msg| content_tag(:li, msg.html_safe) }
-        content_tag(:h2, options[:header_message].html_safe) + content_tag(:p, options[:message].html_safe) + content_tag(:ul, list_items.join.html_safe)
+        content_tag(:h3, options[:header_message].html_safe) + content_tag(:p, options[:message].html_safe) + content_tag(:ul, list_items.join.html_safe)
       end
     end
   end
